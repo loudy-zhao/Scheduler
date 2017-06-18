40117748e695c04add21bc546bf293acd1835d78
Etienne Audet-Cobello <etienneaudet98@outlook.com>
2017-06-18 10:50:11 -0400

    Added send timesheet by email feature.

--- scheduler.py
+++ scheduler.py
@@ -1,23 +1,36 @@
+#Author: Etienne Audet-Cobello
+
 #Imports
 import openpyxl
 import smtplib
 from datetime import date
 from datetime import timedelta
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
+from email.utils import formatdate
+from email import encoders
 
-#Constants
+#User settings constants
+MY_EMAIL = ""
+MY_EMAIL_PASSWORD = ""
+EMAIL_TIMESHEET_RECIPIENT = ""
 SCHEDULE_FILE_NAME = "timesheet.xlsx"
 SCHEDULE_SHEET_NAME = 'Sheet1'
 SCHEDULE_SHEET_WEEK_DATES_BEGINNING_CELL = 'B19'
 SCHEDULE_SHEET_WEEK_DATES_ENDING_CELL = 'B25'
 SCHEDULE_SHEET_WEEK_STARTING_CELL = 'E8'
 SCHEDULE_SHEET_SIGNATURE_DATE_CELL = 'E34'
+EMAIL_PROVIDER = "smtp.gmail.com" # No need to change this.
+EMAIL_PROVIDER_PORT = 587 # No need to change this.
+
+#Other constants
 NUMBER_OF_DAYS_IN_WEEK = 7
 TODAY = date.today()
 
-################### Fill Timesheet Data ###################
+#Formats the date.
+def formatDate(date):
+    return date.strftime("%m/%d/%Y")
 
 scheduleWorkBook = openpyxl.load_workbook(SCHEDULE_FILE_NAME)
 
@@ -28,16 +41,35 @@ daysToSubstract = NUMBER_OF_DAYS_IN_WEEK
 
 for rows in scheduleSheet[SCHEDULE_SHEET_WEEK_DATES_BEGINNING_CELL:SCHEDULE_SHEET_WEEK_DATES_ENDING_CELL]:
     for cell in rows:
-        day = (TODAY - timedelta(days=daysToSubstract)).strftime("%m/%d/%Y")
+        day = formatDate((TODAY - timedelta(days=daysToSubstract)))
         daysToSubstract -= 1
         scheduleSheet[cell.coordinate] = day
 
 #Replaces the 'Week Starting' cell by removing 7 days from today's date.
-weekStarting = (TODAY - timedelta(days=NUMBER_OF_DAYS_IN_WEEK)).strftime("%m/%d/%Y")
+weekStarting = formatDate((TODAY - timedelta(days=NUMBER_OF_DAYS_IN_WEEK)))
 scheduleSheet[SCHEDULE_SHEET_WEEK_STARTING_CELL] = weekStarting
 
 #Replaces the date field of the employee signature area by today's date.
-scheduleSheet[SCHEDULE_SHEET_SIGNATURE_DATE_CELL] = 'Date: ' + TODAY.strftime("%m/%d/%Y")
+scheduleSheet[SCHEDULE_SHEET_SIGNATURE_DATE_CELL] = 'Date: ' + formatDate(TODAY)
 
 #Save all changes to the workbook.
 scheduleWorkBook.save(SCHEDULE_FILE_NAME)
+
+#Create email.
+msg = MIMEMultipart()
+msg['From'] = MY_EMAIL
+msg['To'] = EMAIL_TIMESHEET_RECIPIENT
+msg['Date'] = formatDate(TODAY)
+msg['Subject'] = "Timesheet - Week of : " + weekStarting
+
+part = MIMEBase('application', "octet-stream")
+part.set_payload(open(SCHEDULE_FILE_NAME, "rb").read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename=' + SCHEDULE_FILE_NAME)
+msg.attach(part)
+
+smtp = smtplib.SMTP(EMAIL_PROVIDER, EMAIL_PROVIDER_PORT)
+smtp.starttls()
+smtp.login(MY_EMAIL, MY_EMAIL_PASSWORD)
+smtp.sendmail(MY_EMAIL, EMAIL_TIMESHEET_RECIPIENT, msg.as_string())
+smtp.quit()
